2645176fc4b8e1c219f95ba8ebaf329220e769f1
Vinicius Biondi <viniciusvwxyz@gmail.com>
2018-01-08 21:55:10 -0200

    Update Plant.py

--- capivaraprojects/greeneyes/models/Plant.py
+++ capivaraprojects/greeneyes/models/Plant.py
@@ -1,5 +1,5 @@
 class Plant:
-    def __init__(self, id, scientificName, commonName, diseases=list()):
+    def __init__(self, id=0, scientificName="", commonName="", diseases=list()):
         self.id = id
         self.scientificName = scientificName
         self.commonName = commonName
