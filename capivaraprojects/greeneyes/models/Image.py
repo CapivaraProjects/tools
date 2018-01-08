6a8449c0cd6be392af42f61e81047b90584abe4f
Wendel Hime <wendelhime@hotmail.com>
2018-01-08 21:49:01 -0200

    fix issue5 (#23)

--- capivaraprojects/greeneyes/models/Image.py
+++ capivaraprojects/greeneyes/models/Image.py
@@ -1,3 +1,4 @@
+from capivaraprojects.greeneyes.models.Disease import Disease
 class Image:
     def __init__(self, id=0, disease=Disease(), url="", description="", source=""):
         self.id = id
