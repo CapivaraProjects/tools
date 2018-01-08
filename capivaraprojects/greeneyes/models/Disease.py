1e7b54e07456b227bbb8c5074a32553d7f2ec605
Wendel Hime <wendelhime@hotmail.com>
2018-01-08 21:56:44 -0200

    Revert "add new parameters to relationship (#20)" (#24)
    This reverts commit 98c38798db4e7952c8424dafafc4bc20d0baae2a.

--- capivaraprojects/greeneyes/models/Disease.py
+++ capivaraprojects/greeneyes/models/Disease.py
@@ -1,8 +1,7 @@
 from capivaraprojects.greeneyes.models.Plant import Plant
 class Disease:
-    def __init__(self, id=0, plant=Plant(), scientificName="", commonName="", images=list()):
+    def __init__(self, id=0, plant=Plant(), scientificName="", commonName=""):
         self.id = id
         self.plant = plant
         self.scientificName = scientificName
         self.commonName = commonName
-        self.images=images
