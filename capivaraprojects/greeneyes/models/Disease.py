c3d3d924f2124951874c96362d7109721dea985d
Vinicius Biondi <viniciusvwxyz@gmail.com>
2018-01-08 08:37:06 -0200

    add new parameters to relationship

--- capivaraprojects/greeneyes/models/Disease.py
+++ capivaraprojects/greeneyes/models/Disease.py
@@ -1,6 +1,7 @@
 class Disease:
-    def __init__(self, id=0, plant=Plant(), scientificName="", commonName=""):
+    def __init__(self, id=0, plant=Plant(), scientificName="", commonName="", images=list()):
         self.id = id
         self.plant = plant
         self.scientificName = scientificName
         self.commonName = commonName
+        self.images=images
