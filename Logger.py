8a9af4c97dcba9bdbd01e31cf4333bb38b8b96e0
Giovanni Maciel <du_leite@hotmail.com>
2018-02-19 22:32:29 -0300

    Removed some SQLalchemy excepts

--- Logger.py
+++ Logger.py
@@ -14,7 +14,7 @@ class Logger:
         Method used to create a log and insert into elasticsearch
         """
         """
-        if config != 'TEST':
+        if config != 'TEST': 
         """
         logger = str(json.dumps(models.Logger.Logger(type, message, function, obs, config).__dict__))
         return requests.post(url, data=logger).text
