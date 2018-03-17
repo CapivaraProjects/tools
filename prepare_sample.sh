b166a21d883ecb673a0b4e143e00188ec0a8fa2a
Vinicius Biondi <viniciusvwxyz@gmail.com>
2018-03-17 14:20:12 -0300

    fix script to select files of one specific plant

--- prepare_sample.sh
+++ prepare_sample.sh
@@ -12,7 +12,7 @@ if [ $# -lt "$MISSING_ARGS" ]; then
         SOURCE_DIR 	the original images directory
         TARGET_DIR 	where the samples will be copy
         NMAGES 		how many images you want for this sample
-        TEST 		if test put '1' else '0'"
+        TEST 		if test put '1' else '0'; this ignore the number of images"
 	exit 0
 fi 
 
@@ -22,25 +22,20 @@ NIMAGES=$3 # number of images for each training class
 TEST=$4
 IS_TEST=1
 
-# select just plants that have
-for plant_dir in ${SOURCE_DIR}/*; do # Apple
-	if [ -d "${plant_dir}" ]; then
-		echo "PLANT ${plant_dir}" # plantvillage/thumb/Apple
-		for candidate_class in ${plant_dir}/*; do # healthy
-			if [ -d "${candidate_class}" ]; then
-				if [ "$(ls -1 ${candidate_class}/* | wc -l)" -ge ${NIMAGES} ]; then
-					echo "CAND ${candidate_class}" 	# plantvillage/thumb/Apple/healthy
-					plant_sample=${TARGET_DIR}/${candidate_class}
-					mkdir -p ${plant_sample}
-					echo "coping from ${candidate_class} to ${plant_sample})"
-					if [ "${TEST}" -eq "${IS_TEST}" ] ; then
-						cp -r $(ls -1 ${candidate_class}/* | sort -R | head -2) ${plant_sample}
-					else
-						cp -r $(ls -1 ${candidate_class}/* | sort -R | head -${NIMAGES}) ${plant_sample}
-					fi
-					
-				fi
-			fi
-		done
-	fi
+
+for candidate_class in ${SOURCE_DIR}/*; do
+	if [ -d "${candidate_class}" ]; then
+		if [ "$(ls -1 ${candidate_class}/* | wc -l)" -ge ${NIMAGES} ]; then
+			candidate=${candidate_class:19:${#candidate_class}}
+            echo "CAND ${candidate}"
+			plant_sample=${TARGET_DIR}/${candidate}
+			mkdir -p ${plant_sample}
+            echo "coping from ${candidate_class} to ${plant_sample})"
+	    	if [ "${TEST}" -eq "${IS_TEST}" ] ; then
+			    cp -r $(ls -1 ${candidate_class}/* | sort -R | head -2) ${plant_sample}
+			else
+				cp -r $(ls -1 ${candidate_class}/* | sort -R | head -${NIMAGES}) ${plant_sample}
+			fi				
+		fi
+    fi
 done
